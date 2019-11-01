6df80e301a3028a6d6bd5d301da925f30415970c
Krain Chen <ssssu8@qq.com>
2019-11-01 15:08:14 +0800

    add public key

--- wallet/keypair.go
+++ wallet/keypair.go
@@ -1,27 +1,47 @@
 package wallet
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"fmt"
-	"github.com/joeqian10/neo-gogogo/crypto/ecc"
+	"github.com/joeqian10/neo-gogogo/crypto"
+	"math/big"
 )
 
 type KeyPair struct {
 	PrivateKey []byte
-	PublicKey  ecc.ECPoint
+	PublicKey  ecdsa.PublicKey
 }
 
 func NewKeyPair(privateKey []byte) (key *KeyPair, err error) {
 	length := len(privateKey)
-	if length != 32 && length != 94 && length != 104 {
+	if length != 32 {
 		return nil, fmt.Errorf("argument length is wrong %v", length)
 	}
-
-	key = &KeyPair{privateKey[:32], nil}
-	if length == 32 {
-		//key.PublicKey =  ecc.Secp256r1.G
-	} else {
-
-	}
-
+	priv := ToEcdsa(privateKey)
+	key = &KeyPair{privateKey, priv.PublicKey}
 	return key, nil
+}
+
+// ecsda converts the key to a usable ecsda.PrivateKey for signing data.
+func (p *KeyPair) ToEcdsa() *ecdsa.PrivateKey {
+	return ToEcdsa(p.PrivateKey)
+}
+
+// ecsda converts the private key byte[] to a usable ecsda.PrivateKey for signing data.
+func ToEcdsa(key []byte) *ecdsa.PrivateKey {
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = elliptic.P256()
+	priv.D = new(big.Int).SetBytes(key)
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(key)
+	return priv
+}
+
+func (p *KeyPair) ExportWIF() string {
+	data := make([]byte, 34)
+	data[0] = 0x80
+	copy(data[1:], p.PrivateKey)
+	data[33] = 0x01
+	wif := crypto.Base58CheckEncode(data)
+	return wif
 }
