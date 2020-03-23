d5237d2c8e0b636f9b8ca27048dd836c1ebdae2f
zhuoqian <zhuoqian10@gmail.com>
2020-03-23 18:31:00 +0800

    Added Serialization/Deserialization for StateRoot

--- mpt/stateroot.go
+++ mpt/stateroot.go
@@ -1,8 +1,10 @@
 package mpt
 
+import "github.com/joeqian10/neo-gogogo/helper/io"
+
 //StateRoot truct of StateRoot message
 type StateRoot struct {
-	Version   int    `json:"version"`
+	Version   uint32 `json:"version"`
 	Index     uint32 `json:"index"`
 	PreHash   string `json:"prehash"`
 	StateRoot string `json:"stateroot"`
@@ -10,4 +12,22 @@ type StateRoot struct {
 		InvocationScript   string `json:"invocation"`
 		VerificationScript string `json:"verification"`
 	} `json:"witness"`
+}
+
+func (sr *StateRoot) Deserialize(br *io.BinaryReader) {
+	br.ReadLE(&sr.Version)
+	br.ReadLE(&sr.Index)
+	sr.PreHash = br.ReadVarString()
+	sr.StateRoot = br.ReadVarString()
+	sr.Witness.InvocationScript = br.ReadVarString()
+	sr.Witness.VerificationScript = br.ReadVarString()
+}
+
+func (sr *StateRoot) Serialize(bw *io.BinaryWriter) {
+	bw.WriteLE(sr.Version)
+	bw.WriteLE(sr.Index)
+	bw.WriteVarString(sr.PreHash)
+	bw.WriteVarString(sr.StateRoot)
+	bw.WriteVarString(sr.Witness.InvocationScript)
+	bw.WriteVarString(sr.Witness.VerificationScript)
 }
