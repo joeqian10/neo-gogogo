237d985a02d5e5428c1d9c26d450ad06d6ee6691
zhuoqian <zhuoqian10@gmail.com>
2021-01-18 17:45:21 +0800

    fix typo

--- helper/io/serializable_test.go
+++ helper/io/serializable_test.go
@@ -6,23 +6,23 @@ import (
 	"testing"
 )
 
-type TestSerialzable struct {
+type TestSerializable struct {
 	Flag  bool
 	Value []byte
 }
 
-func (t *TestSerialzable) Serialize(writer *BinaryWriter) {
+func (t *TestSerializable) Serialize(writer *BinaryWriter) {
 	writer.WriteLE(t.Flag)
 	writer.WriteVarBytes(t.Value)
 }
 
-func (t *TestSerialzable) Deserialize(reader *BinaryReader) {
+func (t *TestSerializable) Deserialize(reader *BinaryReader) {
 	reader.ReadLE(&t.Flag)
 	t.Value = reader.ReadVarBytes()
 }
 
 func TestToArray(t *testing.T) {
-	ts := &TestSerialzable{
+	ts := &TestSerializable{
 		Flag:  true,
 		Value: helper.HexToBytes("abcd"),
 	}
@@ -32,7 +32,7 @@ func TestToArray(t *testing.T) {
 
 func TestAsSerializable(t *testing.T) {
 	data := []byte{0x01, 0x02, 0xab, 0xcd}
-	ts := &TestSerialzable{}
+	ts := &TestSerializable{}
 	AsSerializable(ts, data)
 	assert.Equal(t, true, ts.Flag)
 	assert.Equal(t, []byte{0xab, 0xcd}, ts.Value)
